e9f241a143a679fa0230c4c4e22eb31c72d8355a
Eric Wong <e.gin.wong@gmail.com>
2020-11-08 13:11:09 -0500

    refactor: move auction to separate service

--- entities/PlayerActions.js
+++ entities/PlayerActions.js
@@ -82,37 +82,6 @@ module.exports = class PlayerActions {
 
   // returns { buyer, price }
   static auction(UI, players, property, baseCost) {
-    let biddingPlayers;
-    let cost = baseCost;
-    let currentHighestBidPlayer;
-
-    // require at least one player to have bid
-    while (!currentHighestBidPlayer) {
-      biddingPlayers = players.slice();
-      // while players are still in the running
-      while (biddingPlayers.length > 1) {
-        UI.displayPropertyDetails(property);
-        UI.playersInAuction(biddingPlayers);
-        for (let i = 0; i < biddingPlayers.length; i++) {
-          UI.playerInAuction(biddingPlayers[i]);
-          let bid = PlayerActions.numberPrompt(
-            UI,
-            `How much would you like to bid? Minimum bid is ${cost + 1} \n`
-          );
-
-          const biddingPlayer = biddingPlayers[i];
-          if (isNaN(bid) || bid <= cost || bid > biddingPlayer.liquidity) {
-            UI.playerOutOfAuction(biddingPlayer);
-            biddingPlayer.outOfAuction = true;
-          } else {
-            currentHighestBidPlayer = biddingPlayer;
-            cost = bid;
-          }
-        }
-
-        biddingPlayers = biddingPlayers.filter((p) => !p.outOfAuction);
-      }
-    }
-    return { buyer: currentHighestBidPlayer, price: cost };
+    return require('./AuctionService').auction(UI, players, property, baseCost);
   }
 };
--- test/PlayerActions.test.js
+++ test/PlayerActions.test.js
@@ -5,6 +5,7 @@ const mockUIFactory = require('./mocks/UI');
 const { GameState } = require('../entities/GameState');
 const { createPlayerFactory } = require('./testutils');
 const PlayerActions = require('../entities/PlayerActions');
+const AuctionService = require('../entities/AuctionService');
 const config = require('../config/monopolyConfiguration');
 const { cloneDeep } = require('lodash');
 const { calculateLiquidity } = require('../entities/WealthService');
@@ -381,7 +382,7 @@ describe('PlayerActions', () => {
   });
 
   describe('auction', () => {
-    it('re-auctions when no one bids', () => {
+    it('calls the AuctionService', () => {
       const testBaseCost = 100;
       const testProperty = gameState.config.propertyConfig.properties.find(
         (p) => p.id === 'mediterraneanave'
@@ -395,14 +396,7 @@ describe('PlayerActions', () => {
           player
         ),
       }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(1);
-      promptStub.onCall(2).returns(`${testBaseCost}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(4).returns(0);
-      promptStub.onCall(5).returns(0);
+      const auctionServiceStub = sinon.stub(AuctionService, 'auction');
 
       PlayerActions.auction(
         UI,
@@ -410,312 +404,14 @@ describe('PlayerActions', () => {
         testProperty,
         testBaseCost
       );
-      expect(promptStub.callCount).to.equal(
-        6,
-        `Game did not re-auction when no player provided acceptable bids`
-      );
-    });
-    it('continues auction rounds until only one player is left', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 3}`);
-      promptStub.onCall(4).returns(`${testBaseCost + 4}`);
-      promptStub.onCall(5).returns(`${testBaseCost + 5}`);
-      promptStub.onCall(6).returns(0);
-
-      PlayerActions.auction(
-        UI,
-        playersWithLiquidity,
-        testProperty,
-        testBaseCost
-      );
-      expect(promptStub.callCount).to.equal(
-        7,
-        `Game did not continue auction rounds with fewer players`
-      );
-    });
-    it('calls expected UI calls for auction process', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-      const displayPropertyDetailsUISpy = sinon.spy();
-      UI.displayPropertyDetails = displayPropertyDetailsUISpy;
-      const playersInAuctionUISpy = sinon.spy();
-      UI.playersInAuction = playersInAuctionUISpy;
-      const playerInAuctionUISpy = sinon.spy();
-      UI.playerInAuction = playerInAuctionUISpy;
-      const playerOutOfAuctionUISpy = sinon.spy();
-      UI.playerOutOfAuction = playerOutOfAuctionUISpy;
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(0);
-
-      PlayerActions.auction(
-        UI,
-        playersWithLiquidity,
-        testProperty,
-        testBaseCost
-      );
-      expect(displayPropertyDetailsUISpy.calledOnce).to.equal(
-        true,
-        `Game did not announce the property to be auctioned`
-      );
       expect(
-        playersInAuctionUISpy.calledOnceWithExactly(playersWithLiquidity)
-      ).to.equal(
-        true,
-        `Game did not announce the players participating in the auction`
-      );
-      expect(playerInAuctionUISpy.calledThrice).to.equal(
-        true,
-        `Game did not announce each player per auction round`
-      );
-      expect(playerOutOfAuctionUISpy.calledTwice).to.equal(
-        true,
-        `Game did not announce the players who were eliminated from the auction`
-      );
-    });
-    it('filters players out of the auction so that subsequent rounds are shorter', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 3}`);
-      promptStub.onCall(4).returns(0);
-      const expectedWinner = playersWithLiquidity[1];
-      const expectedCost = testBaseCost + 3;
-
-      expect(
-        PlayerActions.auction(
+        auctionServiceStub.calledOnceWithExactly(
           UI,
           playersWithLiquidity,
           testProperty,
           testBaseCost
         )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not filter out players from the auction`
-      );
-    });
-    it('eliminates player if bid is not a number', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns('0.1abc');
-      promptStub.onCall(1).returns(`-`);
-      promptStub.onCall(2).returns(`${testBaseCost + 1}`);
-      const expectedWinner = playersWithLiquidity[2];
-      const expectedCost = testBaseCost + 1;
-
-      expect(
-        PlayerActions.auction(
-          UI,
-          playersWithLiquidity,
-          testProperty,
-          testBaseCost
-        )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not eliminate non-numerical input`
-      );
-    });
-    it('eliminates player if bid is below current lowest bid', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(4).returns(0);
-      const expectedWinner = playersWithLiquidity[2];
-      const expectedCost = testBaseCost + 2;
-
-      expect(
-        PlayerActions.auction(
-          UI,
-          playersWithLiquidity,
-          testProperty,
-          testBaseCost
-        )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not filter out player bid that is below minimum bidding cost`
-      );
-    });
-    it("eliminates player if bid is above player's liquidity", () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${playersWithLiquidity[2].liquidity + 1}`);
-      const expectedWinner = playersWithLiquidity[1];
-      const expectedCost = testBaseCost + 1;
-
-      expect(
-        PlayerActions.auction(
-          UI,
-          playersWithLiquidity,
-          testProperty,
-          testBaseCost
-        )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not filter out player bid that is above player liquidity`
-      );
-    });
-    it('returns buyer and price at the end of the auction', () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 3}`);
-      promptStub.onCall(4).returns(`${testBaseCost + 4}`);
-      promptStub.onCall(5).returns(`${testBaseCost + 5}`);
-      promptStub.onCall(6).returns(0);
-      const expectedWinner = playersWithLiquidity[1];
-      const expectedCost = testBaseCost + 5;
-
-      expect(
-        PlayerActions.auction(
-          UI,
-          playersWithLiquidity,
-          testProperty,
-          testBaseCost
-        )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not return buyer and expected cost`
-      );
-    });
-    it("returns player's highest bid even if the following round, they enter in an invalid bid", () => {
-      const testBaseCost = 100;
-      const testProperty = gameState.config.propertyConfig.properties.find(
-        (p) => p.id === 'mediterraneanave'
-      );
-      const UI = mockUIFactory();
-      const playersWithLiquidity = gameState.players.map((player) => ({
-        ...player,
-        liquidity: calculateLiquidity(
-          gameState,
-          gameState.config.propertyConfig.properties,
-          player
-        ),
-      }));
-
-      const promptStub = sinon.stub(PlayerActions, 'numberPrompt');
-      promptStub.onCall(0).returns(0);
-      promptStub.onCall(1).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(2).returns(`${testBaseCost + 2}`);
-      promptStub.onCall(3).returns(`${testBaseCost + 1}`);
-      promptStub.onCall(4).returns(0);
-      const expectedWinner = playersWithLiquidity[2];
-      const expectedCost = testBaseCost + 2;
-
-      expect(
-        PlayerActions.auction(
-          UI,
-          playersWithLiquidity,
-          testProperty,
-          testBaseCost
-        )
-      ).to.deep.equal(
-        { buyer: expectedWinner, price: expectedCost.toString() },
-        `Auction did not return highest player's bid even though their subsequent bid was not accepted`
-      );
+      ).to.equal(true, `Game did not call the AuctionService`);
     });
   });
 });
