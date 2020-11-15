4d7640954ba5bc0b7812f47d2a7820c5a8d1a904
Eric Wong <e.gin.wong@gmail.com>
2020-11-15 12:53:46 -0500

    refactor: remove extra while loop

--- entities/AuctionService.js
+++ entities/AuctionService.js
@@ -1,35 +1,45 @@
-module.exports = class AuctionSevice {
+module.exports = class AuctionService {
   static auction(UI, players, property, baseCost) {
-    let biddingPlayers;
+    let bids = Array(players.length).fill(0);
     let cost = baseCost;
-    let currentHighestBidPlayer;
+    let biddingPlayer = null; // Hoisted variable used in loop
 
-    // require at least one player to have bid
-    while (!currentHighestBidPlayer) {
-      biddingPlayers = players.slice();
-      // while players are still in the running
-      while (biddingPlayers.length > 1) {
-        UI.displayPropertyDetails(property);
-        UI.playersInAuction(biddingPlayers);
-        for (let i = 0; i < biddingPlayers.length; i++) {
-          UI.playerInAuction(biddingPlayers[i]);
-          let bid = UI.promptNumber(
-            `How much would you like to bid? Minimum bid is ${cost + 1} \n`
-          );
+    // While the count of players bidding is not one..
+    while (bids.filter((bid) => bid > 0).length !== 1) {
+      UI.displayPropertyDetails(property);
+      UI.playersInAuction(
+        // If NOT (-1) THEN bidding player ELSE false
+        bids
+          .map((bid, index) => (~bid ? players[index] : false))
+          .filter((player) => player) // players only
+      );
 
-          const biddingPlayer = biddingPlayers[i];
-          if (isNaN(bid) || bid <= cost || bid > biddingPlayer.liquidity) {
-            UI.playerOutOfAuction(biddingPlayer);
-            biddingPlayer.outOfAuction = true;
-          } else {
-            currentHighestBidPlayer = biddingPlayer;
-            cost = bid;
-          }
+      bids = bids.map((bid, index) => {
+        if (!~bid) return -1; // Player is out of auction
+        if (bid === cost) return bid; // Player is already highest bidder
+        biddingPlayer = players[index];
+        UI.playerInAuction(biddingPlayer);
+        bid = UI.promptNumber(
+          `How much would you like to bid? Minimum bid is $${cost + 1}\n`
+        );
+
+        if (isNaN(bid) || bid <= cost || bid > biddingPlayer.liquidity) {
+          UI.playerOutOfAuction(biddingPlayer);
+          return -1; // Player is out of auction
         }
 
-        biddingPlayers = biddingPlayers.filter((p) => !p.outOfAuction);
+        return (cost = bid);
+      });
+
+      // requires at least one player to have bid
+      // If no bidders.. (GOTO or recursive call)
+      if (bids.filter((bid) => bid > 0).length === 0) {
+        // Start over
+        bids = Array(players.length).fill(0);
+        cost = baseCost;
       }
     }
-    return { buyer: currentHighestBidPlayer, price: cost };
+
+    return { buyer: players[bids.findIndex((bid) => ~bid)], price: cost };
   }
 };
